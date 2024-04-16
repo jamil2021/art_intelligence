7ecbba42cba75a72ad0af47db61fda4ae0999f41
jamil <jamil.bce@gmail.com>
2024-04-16 06:11:42 +0500

    plotted cost function with two different learning rates (alpha).

--- gradient_descent.py
+++ gradient_descent.py
@@ -26,24 +26,13 @@ def gradient_descent(X, y, w, b, alpha=0.05, iterations=100):
         partialJb = 1/m * np.sum(e)
 
         w = w - (alpha * partialJw)
-        b = b - (alpha * partialJb)
+        b = b - (alpha * partialJb)        
 
-        
-
-        if i % 10 == 0:
+        if i % 3 == 0:
             it.append(i)
             costJ.append(Jwb)
             print("Cost after iteration %d: %f" % (i, Jwb))
-
-    plt.plot(it,costJ)
-    plt.title('Gradient Descent Linear Regression J')
-    plt.xlabel('Iterations')
-    plt.ylabel('Cost J')
-    const_line = [0.01985] * len(it)
-    plt.plot(it, const_line, linestyle=':', linewidth=1.0, color='red')
-    plt.show()
-
-    return w,b
+    return w,b,it,costJ
 
 
 # Generate random data for linear regression
@@ -55,18 +44,28 @@ y = 0.3 * X + res
 # Perform gradient descent
 w = 0.2
 b = 0.2
-alpha = 0.005
-iterations = 10000
-w,b = gradient_descent(X, y, w, b, alpha, iterations)
-print(f"w = {w}, b = {b}")
+# alpha = 0.005
+iterations = 300
+w1,b1,it1,costJ1 = gradient_descent(X, y, w, b, 0.05, iterations)
+w2,b2,it2,costJ2 = gradient_descent(X, y, w, b, 0.005, iterations)
+print(f"w = {w1}, b = {b1}")
 # Plotting the results
 plt.figure(figsize=(10,6))
 plt.scatter(X, y, color='blue')
-plt.plot(X, fwb_x(X, w, b), color='red')
-plt.title('Gradient Descent Linear Regression')
+plt.plot(X, fwb_x(X, w1, b1), color='red')
+plt.title('Gradient Descent Linear Regression (alpha=0.05)')
 plt.xlabel('Independent variable')
 plt.ylabel('Dependent variable')
 plt.show()
 
+plt.plot(it1,costJ1, label='alpha=0.05')
+plt.plot(it2,costJ2, label='alpha=0.005')
+plt.title('Gradient Descent Linear Regression J')
+plt.xlabel('Iterations')
+plt.ylabel('Cost J')
+plt.legend()
+# const_line = [0.01985] * len(it)
+# plt.plot(it, const_line, linestyle=':', linewidth=1.0, color='red')
+plt.show()
 
 
